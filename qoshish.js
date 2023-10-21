3f409364e5a9416afcee6856385d87bf0a35c9d9
Doniyorov Doniyor <ddonierov68@gmail.com>
2023-10-21 15:21:37 +0500

    Math

---
+++ qoshish.js
@@ -0,0 +1,24 @@
+let a = 5
+let b = 5
+let c = 5+5
+console.log(a+b);
+
+let d = 6
+let e = 7
+let f = 6+7
+console.log(d+e);
+
+let g = 10
+let l = 5
+let u = 10+9
+console.log(g+l);
+
+let s = 49
+let h = 645
+let m = 49+645
+console.log(s+h);
+
+let z = 324
+let i = 543
+let p = 324+543
+console.log(z+i);
