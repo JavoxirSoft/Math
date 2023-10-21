0cfef16d1660a1613275bfa9a041ca57ec54c3ec
Doniyorov Doniyor <ddonierov68@gmail.com>
2023-10-21 15:22:11 +0500

    Math

---
+++ bolish.js
@@ -0,0 +1,24 @@
+let az = 5
+let ba = 5
+let ca = 5/5
+console.log(az/ba);
+
+let ds = 6
+let ea = 7
+let fl = 6/7
+console.log(ds/ea);
+
+let gl = 10
+let lo = 5
+let uh = 10/9
+console.log(gl/lo);
+
+let sb = 49
+let hm = 645
+let mn = 49/645
+console.log(mn);
+
+let ze = 324
+let ix = 543
+let pz = 324/543
+console.log(ze/ix);
