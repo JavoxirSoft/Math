ed543e7a5a7f178aa3b8fdca5587869d514f25b7
Doniyorov Doniyor <ddonierov68@gmail.com>
2023-10-21 15:24:07 +0500

    Math

---
+++ kopaytirish.js
@@ -0,0 +1,24 @@
+let abb = 5
+let bbb = 5
+let ccc = 5*5
+console.log(abb*bbb);
+
+let dd = 6
+let ee = 7
+let ff = 6*7
+console.log(dd*ee);
+
+let gg = 10
+let ll = 5
+let uu = 10*9
+console.log(gg*ll);
+
+let ss = 49
+let hh = 645
+let mm = 49*645
+console.log(ss*hh);
+
+let zz = 324
+let ii = 543
+let pp = 324*543
+console.log(zz*ii);
