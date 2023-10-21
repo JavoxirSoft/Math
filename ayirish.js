c22c3fde54767605b9d63b40a2b03dd306ffcf68
Doniyorov Doniyor <ddonierov68@gmail.com>
2023-10-21 15:23:12 +0500

    Math

---
+++ ayirish.js
@@ -0,0 +1,24 @@
+let ab = 5
+let bc = 5
+let cd = 5-5
+console.log(ab-bc);
+
+let de = 7
+let ef = 6
+let fa = 7-6
+console.log(de-ef);
+
+let gh = 10
+let lk = 5
+let ul = 10-5
+console.log(gh-lk);
+
+let sx = 645
+let hl = 49
+let mh = 645-49
+console.log(sx-hl);
+
+let zp = 543
+let im = 324
+let pq = 543-324
+console.log(zp-im);
